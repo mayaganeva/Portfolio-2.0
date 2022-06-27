e95ccedcebe140a47e82e2dc1bfa49a7829b60b1
Maya <mayagganeva@gmail.com>
2022-06-27 15:16:18 +0300

    contact form style + tweaks all around

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Auto resize textareas
+
+var tx = document.getElementsByTagName('textarea');
+for (var i = 0; i < tx.length; i++) {
+  tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput(e) {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + '1px';
+}
